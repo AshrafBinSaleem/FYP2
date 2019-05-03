eefe1c8f05882df3b5fb094628ccced7c6f30e07
MAbdullah <MAbdullah@GBNB4245>
2019-05-03 18:22:22 +0800

    added developer chart calculation

--- routes/testpage.js
+++ routes/testpage.js
@@ -13,46 +13,7 @@ router.get('/', function (req, res, next) {
 
   var Sales = mongoose.model('sales', salesSchema, "sales");
 
-  // Sales.aggregate([{
-  //   $group: {
-  //     _id : {MaxMonth: { $max : { $month: "$date"} }, year: { $max : {$year: "$date"} } }
-  //     }
-  // }],function(err, obj)
-  // {
-  //   console.log(obj);
-  // })
-
-
-  // Sales.aggregate([{
-  //   $group: {
-  //     _id : {month: { $month: "$date" }, year: { $year: "$date" }, title: "$title" },
-  //     title : {$first : "$title"} ,
-  //     month : {$first : {$month: "$date"} }, 
-  //     count: { $sum: 1 }
-  //     },
-
-  // },
-  // {$sort : {title: 1, month: 1 }}],function(err, obj)
-  // {
-  //   var d = new Date();
-  //   var n = d.getMonth()+1;
-  //   console.log("Month : ",n);
-  //   console.log(obj.find().distinct('title'));
-  //   console.log(obj);
-  // })
-
-  // var salesprom = Sales.aggregate([{
-  //   $group: {
-  //     _id : {month: { $month: "$date" }, year: { $year: "$date" }, title: "$title" },
-  //     title : {$first : "$title"} ,
-  //     month : {$first : {$month: "$date"} }, 
-  //     count: { $sum: 1 }
-  //     },
-
-  // },
-  // {$sort : {title: 1, month: 1 }}]);
-
-
+  var Product = mongoose.model('product', productSchema, "product"); 
 
   Sales.find().distinct('title', async function (error, titles) {
     if (error) return console.error(error);
@@ -72,12 +33,9 @@ router.get('/', function (req, res, next) {
 
     var d = new Date();
     var n = d.getMonth() + 1;
-    //console.log("N:", n);
+
     var Month = ["'Jan'", "'Feb'" , "'March'", "'April'", "'June'" , "'July'", "'May'" , "'Aug'", "'Stp'", "'Oct'", "'Nov'", "'Dec'"];
-    //console.log(resProm[0][0].count)
-    // salesprom.then(function (doc) {
-    //   console.log(doc)
-    // });
+
     var charDataArra = [];
     var idElement = 0;
     titles.forEach(element => {
@@ -143,128 +101,59 @@ router.get('/', function (req, res, next) {
 
     gametypeout = ["'Month'","'SinglePlayer'", "'MultiPlayer'", "'Both'"]
     gametitlecolumntype = ["'string'","'number'","'number'","'number'"]
-    // resSalesProm[0].forEach(element => {
-    //   console.log(element.productDetails[0].play_Mode)
-    //   if(element.productDetails[0].play_Mode == "SinglePlayer"){
-    //     gameTypeCount[0] += 1;
-    //   }
-    //   else if(element.productDetails[0].play_Mode == "MultiPlayer"){
-    //     gameTypeCount[1] += 1;
-    //   }
-    //   else if(element.productDetails[0].play_Mode == "Both"){
-    //     gameTypeCount[2] += 1;
-    //   }
-      
-    // });
-    
-    console.log(playModeArra);
+
+    /** Developer graph */
+    const resDevelopProm = await Promise.all([
+      Sales.aggregate([{
+        $lookup: {
+          from : "product" ,
+          localField : "title",
+          foreignField : "title",
+          as: "productDetails"
+        }},{
+        $group: {
+          _id: { month: { $month: "$date" }, year: { $year: "$date" }, developer: "$productDetails.developer" },
+          developer: { $first: "$productDetails.developer" },
+          month: { $first: { $month: "$date" } },
+          count: { $sum: 1 }
+        },
+
+      } ])
+    ]);
+
+    var resDeveloper = await Promise.all([
+      Product.aggregate([{
+        $group: {
+          _id: { developer: "$developer" },
+          developer: { $first: "$developer" }
+        },
+      } ])
+    ]);
+
+    console.log(resDeveloper[0]);
+    var developDataArra = [];
+    var idElement = 0;
+    resDeveloper[0].forEach(element => {
+      tmpArra = new Array(12).fill(0);;
+      var monthInd = 1;
+      //console.log(element); 
+      resDevelopProm[0].forEach(charData => {
+        if (charData.developer == element.developer) {
+          tmpArra[charData.month - 1] = charData.count 
+
+        }
+
+      });
+      developDataArra.push(tmpArra);
+    });
+
+    console.log(resDevelopProm[0]);
+    console.log(developDataArra);
 
     res.render('testpage', { tit: titleNew, mon : Month, data: charDataArra ,coltype : columnType, playmodedata : playModeArra, gametitle : gametypeout, gamecoltype : gametitlecolumntype });
 
   })
 
-  // Sales.find( async function (error, sales) {
-  //   if (error) return console.error(error);
-  //   const resProm = await Promise.all([
-  //     Sales.aggregate([{
-  //       $lookup: {
-  //         from : "product" ,
-  //         localField : "title",
-  //         foreignField : "title",
-  //         as: "productDetails"
-        
-  //       }
-
-  //     } ])
-  //   ]);
-
-  //   console.log(resProm[0][0].productDetails[0].play_Mode);
-
-  //   var d = new Date();
-  //   var n = d.getMonth() + 1;
-  //   //console.log("N:", n);
-  //   var Month = ["'Jan'", "'Feb'" , "'March'", "'April'", "'June'" , "'July'", "'May'" , "'Aug'", "'Stp'", "'Oct'", "'Nov'", "'Dec'"];
-  //   //console.log(resProm[0][0].count)
-  //   // salesprom.then(function (doc) {
-  //   //   console.log(doc)
-  //   // });
-  //   // var gameType = ['Single', 'Multiplayer', 'Both']; 
-  //   var gameTypeCount = [0,0,0];
-  //   // var charDataArra = [];
-  //   // var idElement = 0;
-  //   resProm[0].forEach(element => {
-  //     console.log(element.productDetails[0].play_Mode)
-  //     if(element.productDetails[0].play_Mode == "SinglePlayer"){
-  //       console.log("1");
-  //       gameTypeCount[0] += 1;
-  //     }
-  //     else if(element.productDetails[0].play_Mode == "MultiPlayer"){
-  //       console.log("2");
-  //       gameTypeCount[1] += 1;
-  //     }
-  //     else if(element.productDetails[0].play_Mode == "Both"){
-  //       console.log("3");
-  //       gameTypeCount[2] += 1;
-  //     }
-  //     console.log(gameTypeCount)
-  //     // tmpArra = new Array(12).fill(0);;
-  //     // var monthInd = 1;
-  //     // //console.log(element); 
-  //     // resProm[0].forEach(charData => {
-  //     //   if (charData.title == element) {
-  //     //     tmpArra[charData.month - 1] = charData.count 
-
-  //     //   }
-
-  //     // });
-  //     // charDataArra.push(tmpArra);
-  //   });
-
-  //   // var columnType = [];
-  //   // var titleNew = []
-  //   // columnType.push("'string'");
-  //   // titleNew.push("'Month'");
-
-  //   // for (let index = 0; index < titles.length; index++) {
-  //   //  titleNew.push( "'" + titles[index] +"'");
-  //   //  columnType.push("'number'");
-  //   // }
-
-  //   // console.log(charDataArra);
-  //   // console.log(Month);
-  //   // console.log(titles);
-    
-  //   // res.render('testpage', { tit: titleNew, mon : Month, data: charDataArra ,coltype : columnType});
-
-  // })
-  //   const res = await Promise.all([
-  //     Sales.find().distinct('title'),
-  //     Sales.aggregate([{
-  //       $group: {
-  //         _id : {month: { $month: "$date" }, year: { $year: "$date" }, title: "$title" },
-  //         title : {$first : "$title"} ,
-  //         month : {$first : {$month: "$date"} }, 
-  //         count: { $sum: 1 }
-  //         },
-
-  //     },{$sort : {title: 1, month: 1 }}])
-  //   ]);
-
-  // console.log(res);
-  //   var Product = mongoose.model('product', productSchema,"product");
-
-
-  //     Product.findOne({title: "Sekiro"}, function(error, product){
-  //         if (error) return console.error(error);
-  //         console.log(product);
-  //         var Company = mongoose.model('company', companySchema,"company");
-
-  //         Company.findOne({_id: product.publisher}, function(error, company) {
-  //             console.log(company);
-  //             res.render('testpage',{mgobj: product});
-  //         });
-
-  // });
 
 
 });
